1b5370997059248249860ea3b8ab7c91871aece2
root <root@DESKTOP-H2KN85O>
2024-12-23 16:33:13 -0500

    Solution to Advent of Code 2024 Day 23

---
+++ Day 23/Advent of Code 2024 Day 23/src/Part1.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+public class Part1 {
+    static Map<String, Set<String>> graph = new HashMap<>();
+    private static int getTripleConnectedComputers() {
+        Set<String> triples = new HashSet<>();
+        for(Map.Entry<String, Set<String>> entry : graph.entrySet()) {
+
+            List<String> neighbors = entry.getValue().stream().toList();
+            for(int i = 0; i < neighbors.size(); i++) {
+                for(int j = i + 1; j < neighbors.size(); j++) {
+                    String source = entry.getKey();
+                    String neighbor1 = neighbors.get(i);
+                    String neighbor2 = neighbors.get(j);
+
+                    if(graph.get(neighbor1).contains(neighbor2) && (source.charAt(0) == 't' ||
+                                                                    neighbor1.charAt(0) == 't' ||
+                                                                    neighbor2.charAt(0) == 't')) {
+                        List<String> nodes = new ArrayList<>();
+                        nodes.add(entry.getKey());
+                        nodes.add(neighbors.get(i));
+                        nodes.add(neighbors.get(j));
+                        Collections.sort(nodes);
+
+                        StringBuilder keySB = new StringBuilder();
+                        for(String node : nodes) {
+                            keySB.append(node);
+                        }
+
+                        String key = keySB.toString();
+                        if (triples.contains(key)) {
+                            continue;
+                        }
+                        triples.add(key);
+                    }
+                }
+            }
+        }
+
+        return triples.size();
+    }
+
+    public static void main(String[] args) {
+        try {
+            File f = new File("C:\\Users\\langd\\OneDrive\\Desktop\\Advent of Code 2024 Day 23\\src\\input.txt");
+            Scanner scanner = new Scanner(f);
+
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String node1 = line.substring(0,2);
+                String node2 = line.substring(3);
+
+                graph.computeIfAbsent(node1, k -> new HashSet<>()).add(node2);
+                graph.computeIfAbsent(node2, k -> new HashSet<>()).add(node1);
+            }
+
+            System.out.println("Triple Connected Components with T: " + getTripleConnectedComputers());
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+}
---
+++ Day 23/Advent of Code 2024 Day 23/src/Part2.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.io.*;
+
+public class Part2 {
+    public static Map<String, Set<String>> graph = new HashMap<>();
+    public static Set<String> maximumConnectedCliques = new HashSet<>();
+
+    // Code inspired by: https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
+    private static void getLargestConnectedClique(Set<String> R, Set<String> P, Set<String> X) {
+        // If P and X are the empty Set, take R as a maximal clique and check
+        // if it is the largest maximal clique.
+        if(P.isEmpty() && X.isEmpty()) {
+            if(R.size() > maximumConnectedCliques.size()) {
+                maximumConnectedCliques = R;
+            }
+            return;
+        }
+
+        // Chose a pivot from the Union of P and X
+        String pivot = "";
+        Iterator<String> pIter = P.iterator();
+        Iterator<String> xIter = X.iterator();
+        if(pIter.hasNext()) {
+            pivot = pIter.next();
+        } else {
+            pivot = xIter.next();
+        }
+
+        Set<String> pCopy = new HashSet<>(P);
+        for(String vertex : pCopy) {
+            if(graph.containsKey(pivot) && graph.get(pivot).contains(vertex)) {
+                continue;
+            }
+
+            // The next R Set we use
+            Set<String> newR = new HashSet<>(R);
+            newR.add(vertex);
+
+            // The next P Set we use
+            Set<String> newP = new HashSet<>();
+            for(String v : graph.get(vertex)) {
+                if(P.contains(v)) {
+                    newP.add(v);
+                }
+            }
+            // The next X Set we use
+            Set<String> newX = new HashSet<>();
+            for(String v : graph.get(vertex)) {
+                if(X.contains(v)) {
+                    newX.add(v);
+                }
+            }
+            // BronKerboschWithPivot(R U v, P n neighbors(v), X n neighbors(v))
+            getLargestConnectedClique(newR, newP, newX);
+            // Remove vertex from P
+            P.remove(vertex);
+            // Add vertex to X
+            X.add(vertex);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            File f = new File("C:\\Users\\langd\\OneDrive\\Desktop\\Advent of Code 2024 Day 23\\src\\input.txt");
+            Scanner scanner = new Scanner(f);
+
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String node1 = line.substring(0,2);
+                String node2 = line.substring(3);
+
+                graph.computeIfAbsent(node1, k -> new HashSet<>()).add(node2);
+                graph.computeIfAbsent(node2, k -> new HashSet<>()).add(node1);
+            }
+
+            long start = System.currentTimeMillis();
+            getLargestConnectedClique(new HashSet<>(), graph.keySet(), new HashSet<>());
+            List<String> nodes = new ArrayList<>(maximumConnectedCliques);
+            Collections.sort(nodes);
+            String password = String.join(",", nodes);
+            System.out.println("Password: " + password);
+            System.out.println("Time taken: " + (System.currentTimeMillis() - start));
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+}
